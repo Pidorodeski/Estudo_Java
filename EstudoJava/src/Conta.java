8ed534203f6ea270325a70e4d71296d92d06b048
Cristian <cristian.00.bill@gmail.com>
2022-01-08 19:42:58 -0300

    refs#00000 - Implementado codigos

--- EstudoJava/src/Conta.java
+++ EstudoJava/src/Conta.java
@@ -3,12 +3,12 @@ public class Conta {
 	private int agencia;
 	private int conta;
 	private double saldo;
-	private String titular;
+	Cliente titular;
 
 	public int getAgencia() {
 		return agencia;
 	}
-	
+
 	public void setAgencia(int agencia) {
 		this.agencia = agencia;
 	}
@@ -16,7 +16,7 @@ public class Conta {
 	public int getConta() {
 		return conta;
 	}
-	
+
 	public void setConta(int conta) {
 		this.conta = conta;
 	}
@@ -47,7 +47,6 @@ public class Conta {
 			this.saldo -= valor;
 			System.out.println("Realizado o saque de R$ " + valor + " para o saldo de R$ " + saldoOld + ".");
 			System.out.println("Novo saldo para a conta " + this.conta + " é de R$ " + this.saldo + ".");
-			System.out.println();
 			return true;
 		} else {
 			System.out.println("Sque não realizado, verifique o valor do saque ou o saldo em conta e tente novamente.");
@@ -62,6 +61,15 @@ public class Conta {
 		this.retiraSaldo(valor);
 		deposita.adicionaSaldo(valor);
 		return true;
+	}
+	
+	public void mostraConta() {
+		System.out.println("Nome usuario: "+this.titular.getNome());
+		System.out.println("CPF: "+this.titular.getCpf());
+		System.out.println("Profissao: "+this.titular.getProfissao());
+		System.out.println("Agencia: "+this.agencia+". Conta: "+this.conta+".");
+		System.out.println("Saldo em conta: "+this.saldo);
+		System.out.println();
 	}
 
 }
--- EstudoJava/src/TestaInteracao.java
+++ EstudoJava/src/TestaInteracao.java
@@ -3,10 +3,23 @@ public class TestaInteracao {
 	public static void main(String[] args) {
 		Conta contaCristian = new Conta();
 		Conta contaDaniela = new Conta();
-		
+		Cliente cristian = new Cliente();
+		Cliente daniela = new Cliente();
+
+		cristian.setNome("Cristian Rocha Pidorodeski");
+		cristian.setCpf("001.001.001-01");
+		cristian.setProfissao("Programador");
+
+		daniela.setNome("Daniela Ferreira de Oliveira");
+		daniela.setCpf("002.002.002-04");
+		daniela.setProfissao("Programadora");
+
+		contaCristian.titular = cristian;
+		contaDaniela.titular = daniela;
+
 		contaCristian.setAgencia(001);
 		contaCristian.setConta(001);
-		
+
 		contaDaniela.setAgencia(002);
 		contaDaniela.setConta(002);
 
@@ -16,7 +29,10 @@ public class TestaInteracao {
 		contaCristian.retiraSaldo(0);
 
 		contaCristian.transfereSaldo(500, contaDaniela);
-		
+
 		contaDaniela.adicionaSaldo(0);
+		
+		contaCristian.mostraConta();
+		contaDaniela.mostraConta();
 	}
 }
