45c025b5b0e45073cb1fafc856ffb6f402a37732
Cristian <cristian.00.bill@gmail.com>
2022-01-08 19:16:09 -0300

    refs#00000

--- EstudoJava/src/Conta.java
+++ EstudoJava/src/Conta.java
@@ -8,35 +8,60 @@ public class Conta {
 	public int getAgencia() {
 		return agencia;
 	}
+	
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
 
 	public int getConta() {
 		return conta;
 	}
 	
-	public boolean adicionaSaldo(double valor) {
-		if(valor > 0) {
-			this.saldo += valor;
-			return true;
-		}else {
-			return false;
-		}
-				
+	public void setConta(int conta) {
+		this.conta = conta;
 	}
-	
-	public boolean retiraSaldo(double valor) {
-		if(valor > 0 && this.saldo >= valor) {
-			double valorOld = this.saldo;
-			this.saldo -= valor;
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public boolean adicionaSaldo(double valor) {
+		if (valor > 0) {
+			double saldoOld = this.saldo;
+			this.saldo += valor;
+			System.out.println("Adicionaro o valor R$ " + valor + " para o saldo R$ " + saldoOld + ".");
+			System.out.println("Novo saldo para a conta " + this.conta + " é de R$ " + this.saldo + ".");
+			System.out.println();
 			return true;
 		} else {
+			System.out.println("Não possivel adicionar o saldo pois o valor " + valor + " não atende aos critérios.");
+			System.out.println("O saldo da conta " + this.conta + " ainda é de R$ " + this.saldo + ".");
+			System.out.println();
 			return false;
 		}
 	}
-	
-	public boolean transfereSaldo( double valor, Conta deposita) {
-		
-		
-		return true;
-		
+
+	public boolean retiraSaldo(double valor) {
+		if (valor > 0 && this.saldo >= valor) {
+			double saldoOld = this.saldo;
+			this.saldo -= valor;
+			System.out.println("Realizado o saque de R$ " + valor + " para o saldo de R$ " + saldoOld + ".");
+			System.out.println("Novo saldo para a conta " + this.conta + " é de R$ " + this.saldo + ".");
+			System.out.println();
+			return true;
+		} else {
+			System.out.println("Sque não realizado, verifique o valor do saque ou o saldo em conta e tente novamente.");
+			System.out.println("O saldo da conta " + this.conta + " ainda é de R$ " + this.saldo + ".");
+			System.out.println();
+			return false;
+		}
 	}
+
+	public boolean transfereSaldo(double valor, Conta deposita) {
+		System.out.println("Transferência realizada com sucesso:");
+		this.retiraSaldo(valor);
+		deposita.adicionaSaldo(valor);
+		return true;
+	}
+
 }
--- EstudoJava/src/TestaInteracao.java
+++ EstudoJava/src/TestaInteracao.java
@@ -1,6 +1,22 @@
 
 public class TestaInteracao {
 	public static void main(String[] args) {
+		Conta contaCristian = new Conta();
+		Conta contaDaniela = new Conta();
 		
+		contaCristian.setAgencia(001);
+		contaCristian.setConta(001);
+		
+		contaDaniela.setAgencia(002);
+		contaDaniela.setConta(002);
+
+		contaCristian.adicionaSaldo(1000);
+		contaCristian.adicionaSaldo(500);
+
+		contaCristian.retiraSaldo(0);
+
+		contaCristian.transfereSaldo(500, contaDaniela);
+		
+		contaDaniela.adicionaSaldo(0);
 	}
 }
